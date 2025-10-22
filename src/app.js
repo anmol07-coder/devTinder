ff6832c359e34466e75ca0fbcdf4c1d7a16b06aa
Anmol Tyagi <tyagiji2005@gmail.com>
2025-10-22 20:48:47 +0530

    Dynamic data in signup API

--- src/app.js
+++ src/app.js
@@ -5,15 +5,12 @@ const User = require("./models/user");
 
 const app = express();
 
+app.use(express.json());
 
 // Creating a signup API because first a user must have to signup or we can say crate account on the app
+
 app.post("/signup",async (req,res)=>{
-    const userObj ={
-        firstName : "Anmol",
-        lastName : "Tyagi",
-        emailId : "tyagiji2005@gmail.com",
-        password : "anmol@tyagi"
-    }
+    const userObj =req.body;
 
     // Creating new instance of our User model
     const user = new User(userObj);
@@ -31,7 +28,7 @@ app.post("/signup",async (req,res)=>{
 connectDB().then(()=>{
     console.log("Database connection established...");
     app.listen(7777,()=>{
-    console.log("Server is successfullt started at port 7777...")
+    console.log("Server is successfully started at port 7777...")
 });
 }).catch((err)=>{
     console.log("Database cannot be connected...");
